435295e59479f24089636baff3d373501e61b61c
Daniil <daniilborisov593@gmail.com>
2025-01-30 20:52:12 +0300

    deleted files

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,3 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
-
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
