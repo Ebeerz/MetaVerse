7f48c6f3b8a8a7e927fd4203cf03c14accdef08a
Daniil <daniilborisov593@gmail.com>
2025-01-31 05:35:32 +0300

    sliders added

---
+++ src/cards.ts
@@ -0,0 +1,12 @@
+// const cardsSection = document.querySelector('.about__cards') as HTMLElement;
+// let activeCard = cardsSection?.querySelector('.card--active') as HTMLElement;
+
+// const onCardClick = (evt: Event, callback: Function) => {
+//     if (activeCard != evt.target) {
+//         activeCard.classList.remove('card--active');
+//         activeCard = evt.target as HTMLElement;
+//         activeCard.classList.add('card--active')
+//         document.querySelector('.about__information--active')?.classList.remove('about__information--active');
+//         document.querySelector(`.about__information#${activeCard.id}`)?.classList.add('about__information--active');
+//     }
+// }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,41 @@
 import './style.css'
+import { Slider } from './slider'
 
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let aboutSliderElement = document.querySelector('.about__information--active .about__slider .slider') as HTMLElement;
+let aboutSlider = new Slider(aboutSliderElement, false, true)
+
+const roboticsCase = document.querySelector('.case__robotics-slider .slider') as HTMLElement;
+const roboticsCaseSlider = new Slider(roboticsCase, false, true)
+
+const festivalCase = document.querySelector('.case__festival-slider .slider') as HTMLElement;
+const festivalCaseSlider = new Slider(festivalCase, false, true)
+
+const cardsSection = document.querySelector('.about__cards') as HTMLElement;
+let activeCard = cardsSection?.querySelector('.card--active') as HTMLElement;
+
+
+const onCardClick = (evt: Event) => {
+    if (activeCard != evt.target) {
+        activeCard.classList.remove('card--active');
+        activeCard = evt.target as HTMLElement;
+        activeCard.classList.add('card--active')
+
+        let aboutInfoActive = document.querySelector('.about__information--active') as HTMLElement
+        aboutInfoActive.classList.remove('about__information--active');
+
+        let newAboutInfoActive = document.querySelector(`.about__information#${activeCard.id}`) as HTMLElement
+        newAboutInfoActive.classList.add('about__information--active');
+
+        aboutSlider.deleteSlider();
+        aboutSliderElement = document.querySelector('.about__information--active .about__slider .slider') as HTMLElement;
+        aboutSlider = new Slider(aboutSliderElement, false, true)
+        aboutSlider.startTimer()
+    }
+}
+
+aboutSlider.startTimer()
+roboticsCaseSlider.startTimer()
+festivalCaseSlider.startTimer()
+
+cardsSection.addEventListener('click', onCardClick)
+// здесь должна вызываться ф-ия 
